6d3b36eb53537c116ecada0e1c017a79a5e4db0f
BppleMan <1286363484@qq.com>
2016-04-06 12:02:35 +0800

    Initial Commit

---
+++ CPP贪吃蛇/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  CPP贪吃蛇
+//
+//  Created by BppleMan‘s Mac on 16/4/6.
+//  Copyright © 2016年 BppleMan‘s Mac. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
